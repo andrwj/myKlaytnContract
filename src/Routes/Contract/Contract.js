1cbbd3f2c4ca1ee411b61f35e4f3ba9d62ad5de6
A.J <andrwj@gmail.com>
2019-06-04 16:36:49 +0900

    comment out unused tab

--- src/Routes/Contract/Contract.js
+++ src/Routes/Contract/Contract.js
@@ -286,37 +286,37 @@ class Contract extends Component {
                   </div>
                 )}
               </TabPanel>
-              <TabPanel className={styles.tabPanel}>
-                <h3 className={styles.h3}>Contact Address</h3>
-                <form>
-                  <input
-                    className={styles.input}
-                    style={{ marginBottom: 30 }}
-                    type="text"
-                  />
-                </form>
-                <h3 className={styles.h3}>Select Existing Contract</h3>
-                <form>
-                  <input
-                    className={styles.input}
-                    style={{ marginBottom: 30 }}
-                    type="text"
-                    placeholder="Select a Contract"
-                  />
-                </form>
-                <h3 className={styles.h3}>ABI / JSON Interface</h3>
-                <form>
-                  <textarea className={styles.textarea} type="text" />
-                  {this.every('isAuthorized', 'gotDeployedContract') && (
-                    <input
-                      className={styles.submit}
-                      style={{ margin: 0 }}
-                      type="submit"
-                      value="Access"
-                    />
-                  )}
-                </form>
-              </TabPanel>
+              {/*<TabPanel className={styles.tabPanel}>*/}
+              {/*  <h3 className={styles.h3}>Contact Address</h3>*/}
+              {/*  <form>*/}
+              {/*    <input*/}
+              {/*      className={styles.input}*/}
+              {/*      style={{ marginBottom: 30 }}*/}
+              {/*      type="text"*/}
+              {/*    />*/}
+              {/*  </form>*/}
+              {/*  <h3 className={styles.h3}>Select Existing Contract</h3>*/}
+              {/*  <form>*/}
+              {/*    <input*/}
+              {/*      className={styles.input}*/}
+              {/*      style={{ marginBottom: 30 }}*/}
+              {/*      type="text"*/}
+              {/*      placeholder="Select a Contract"*/}
+              {/*    />*/}
+              {/*  </form>*/}
+              {/*  <h3 className={styles.h3}>ABI / JSON Interface</h3>*/}
+              {/*  <form>*/}
+              {/*    <textarea className={styles.textarea} type="text" />*/}
+              {/*    {this.every('isAuthorized', 'gotDeployedContract') && (*/}
+              {/*      <input*/}
+              {/*        className={styles.submit}*/}
+              {/*        style={{ margin: 0 }}*/}
+              {/*        type="submit"*/}
+              {/*        value="Access"*/}
+              {/*      />*/}
+              {/*    )}*/}
+              {/*  </form>*/}
+              {/*</TabPanel>*/}
             </Tabs>
           </div>
         </div>
